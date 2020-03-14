87d4493202ebd294799c778861e7da2f9000c4bf
jimmoua1 <tsheejmaus@yahoo.com>
2020-03-13 17:50:35 -0700

    installed node dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      
+      <div>
+      <Route exact path="/" component={MovieList} />
+      </div>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,9 +4,13 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
+      <NavLink key={movie.id} to={'/Movies/${movie.id}'}>
       <span className="saved-movie">{movie.title}</span>
+      </NavLink>
     ))}
+    <Link to = "/">
     <div className="home-button">Home</div>
+    </Link>
   </div>
 );
 
